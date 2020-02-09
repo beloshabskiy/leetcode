73e14b7d89e3c01bb3a00a85e06793a619274670
Evgeniy Beloshabskiy <beloshabskiy@gmail.com>
2020-02-09 14:47:50 +0300

    Problem 162

---
+++ src/main/java/leetcode/Solution162.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * 162. Find Peak Element
+ * A peak element is an element that is greater than its neighbors.
+ * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+ * The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ */
+public class Solution162 {
+
+    public int findPeakElement(int[] nums) {
+        if (isPeak(0, nums)) {
+            return 0;
+        } else if (isPeak(nums.length - 1, nums)) {
+            return nums.length - 1;
+        }
+
+        int left = 1;
+        int right = nums.length - 2;
+
+        while (right - left > 1) {
+            int mid = (left + right) >>> 1;
+            if (isPeak(mid, nums)) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return isPeak(left, nums) ? left : right;
+    }
+
+    private boolean isPeak(int index, int[] nums) {
+        if (nums.length == 1) {
+            return index == 0;
+        }
+        if (index == 0) {
+            return nums[0] > nums[1];
+        } else if (index == nums.length - 1) {
+            return nums[index] > nums[index - 1];
+        } else {
+            return nums[index] > nums[index - 1] && nums[index] > nums[index + 1];
+        }
+    }
+}
---
+++ src/test/java/leetcode/Solution162Test.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Solution162Test {
+
+    @Test
+    void shouldHandleBasicCase1() {
+        Solution162 underTest = new Solution162();
+        final int peakElement = underTest.findPeakElement(new int[]{1, 2, 3, 1});
+        Assertions.assertEquals(2, peakElement);
+    }
+
+    @Test
+    void shouldHandleBasicCase2() {
+        Solution162 underTest = new Solution162();
+        final int peakElement = underTest.findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4});
+        Assertions.assertTrue(peakElement == 1 || peakElement == 5);
+    }
+
+    @Test
+    void shouldHandleAscSortedArray() {
+        Solution162 underTest = new Solution162();
+        final int peakElement = underTest.findPeakElement(new int[]{1, 2, 3, 4});
+        Assertions.assertEquals(3, peakElement);
+    }
+
+    @Test
+    void shouldHandleDescSortedArray() {
+        Solution162 underTest = new Solution162();
+        final int peakElement = underTest.findPeakElement(new int[]{4, 3, 2, 1, 0});
+        Assertions.assertEquals(0, peakElement);
+    }
+
+    @Test
+    void shouldHandleSingleElementArray() {
+        Solution162 underTest = new Solution162();
+        final int peakElement = underTest.findPeakElement(new int[]{4});
+        Assertions.assertEquals(0, peakElement);
+    }
+}
